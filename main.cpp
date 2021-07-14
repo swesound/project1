be546364cc6bb39f43da30ecee276cb275187496
swesound <87410962+swesound@users.noreply.github.com>
2021-07-14 12:16:42 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num1, num2, product;
+    cout << "Enter two numbers:\n";
+    cin >> num1 >> num2;
+    product = num1 * num2;
+    cout << "Product =" << product;
+    return 0;
+}
